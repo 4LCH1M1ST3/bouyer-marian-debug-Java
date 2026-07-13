605a536019379c0f399e3f05ae679a08c857431b
4LCH1M1ST3 <alchi.internet@gmail.com>
2026-07-14 00:30:20 +0200

    javadoc update

--- src/com/hemebiotech/analytics/Main.java
+++ src/com/hemebiotech/analytics/Main.java
@@ -4,16 +4,12 @@ import java.util.Map;
 import java.util.List;
 import java.io.IOException;
 
-/** Appelle les méthodes qui réalisent les différentes actions de lecture, de traitement et d'ecriture des données. 
+/** 
+* Appelle les méthodes qui réalisent les différentes actions de lecture, de traitement et d'ecriture des données. 
 *
 * reader           : Lecteur de symptome.
 * writer           : Écrivain de symptome.
-* analyticsCounter : Instance qui permet de faire appel à 5 fonctions : demander la récupération des symptomes depuis le fichier, 
-*                    effectuer la récupération des symptomes vers une liste de type chaine de caractère, compter les occurences à l'aide d'une HashMap, 
-*                    trier les les symptomes par ordre alphabétique avec une TreeMap, et demander l'écriture des symptomes occurés et triés dans un fichier.
-* symptoms         : Contient les données sous forme de liste non triée et doublonnée du fichier source.
-* countedSymptoms  : Contient les données sous forme de HashMap non triée avec occurences issue de la liste symptoms.
-* sortedSymptoms   : Contient les données sous forme de TreeMap triée avec occurences issue de la HashMap countedSymptoms.
+* analyticsCounter : Instance qui permet de faire appel aux fonctions de traitement des symptomes.
 *
 * @see ISymptomReader
 * @see ReadSymptomDataFromFile
